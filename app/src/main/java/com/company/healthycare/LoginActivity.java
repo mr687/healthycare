174c22d7ffa1ac2813456a3ff7584b5b49d52b75
mr687 <davinomoehdanino@gmail.com>
2019-03-01 17:12:21 +0700

    Add login and register

--- app/src/main/java/com/company/healthycare/LoginActivity.java
+++ app/src/main/java/com/company/healthycare/LoginActivity.java
@@ -1,17 +1,32 @@
 package com.company.healthycare;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class LoginActivity extends AppCompatActivity
 implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    EditText txtEmail,txtPassword;
     TextView textClickCreate;
     Button btnSignIn;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +34,62 @@ implements View.OnClickListener {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progress_bar);
         btnSignIn = findViewById(R.id.btn_sign_in);
         textClickCreate = findViewById(R.id.txt_click_create);
+        txtPassword = findViewById(R.id.txt_password);
+        txtEmail = findViewById(R.id.txt_email);
 
 //        Click
         textClickCreate.setOnClickListener(this);
         btnSignIn.setOnClickListener(this);
     }
+    private void doSIgnIn(){
+        progressBar.setVisibility(View.VISIBLE);
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
 
+        if(email.isEmpty()){
+            txtEmail.setError("Email is required.");
+            txtEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            txtEmail.setError("Email format is incorrect.");
+            txtEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            txtPassword.setError("Password is required.");
+            txtPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent it = new Intent(LoginActivity.this,MainActivity.class);
+                    it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(it);
+                }else{
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG);
+                    return;
+                }
+            }
+        });
+    }
     @Override
     public void onClick(View v) {
         if(v == textClickCreate){
             startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
         }else if(v == btnSignIn){
-            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            doSIgnIn();
         }
     }
 }
--- app/src/main/java/com/company/healthycare/SignUpActivity.java
+++ app/src/main/java/com/company/healthycare/SignUpActivity.java
@@ -1,14 +1,34 @@
 package com.company.healthycare;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity
 implements View.OnClickListener {
+    FirebaseAuth mAuth;
+    EditText txtEmail,txtPassword;
     TextView textClickLogin;
+    Button btnSignUp;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +37,66 @@ implements View.OnClickListener {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        mAuth = FirebaseAuth.getInstance();
+
         textClickLogin = findViewById(R.id.txt_click_login);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+        txtEmail = findViewById(R.id.txt_email);
+        txtPassword = findViewById(R.id.txt_password);
+        progressBar = findViewById(R.id.progress_bar);
 
         textClickLogin.setOnClickListener(this);
+    }
+    private void doRegister(){
+        progressBar.setVisibility(View.VISIBLE);
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            txtEmail.setError("Email is required.");
+            txtEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            txtEmail.setError("Email format is incorrect.");
+            txtEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            txtPassword.setError("Password is required.");
+            txtPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Account registered successfull.",Toast.LENGTH_LONG);
+                    finish();
+                }else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        txtEmail.setError("Email is already.");
+                        txtEmail.requestFocus();
+                        return;
+                    }else{
+                        Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG);
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         if(v == textClickLogin){
             finish();
+        }
+        else if(v == btnSignUp){
+            doRegister();
         }
     }
 }
